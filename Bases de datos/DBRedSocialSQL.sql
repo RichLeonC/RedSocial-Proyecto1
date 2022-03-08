54439b3c6f60bc3b071bcb6820ad22af3bdaa072
IssaAC00 <algueracmp@gmail.com>
2022-03-07 20:58:58 -0600

    Update DBRedSocialSQL.sql

--- Bases de datos/DBRedSocialSQL.sql
+++ Bases de datos/DBRedSocialSQL.sql
@@ -1,2 +1,14 @@
 create database dbRedSocialSQL;
 
+use dbRedSocialSQL;
+create table usuario
+(
+	correoElectronico varchar(150) primary key not null,
+	nombre varchar(50),
+    apellido varchar (50),
+    fechaNacimiento date,
+    clave varchar (200),
+	intereses varchar (200),
+    descripcionGeneral varchar (200),
+    hobies varchar(200)
+); 
