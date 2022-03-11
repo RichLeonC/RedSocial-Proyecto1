efed60a8ca67610dd4393684dd844cc6697b52a8
IssaAC00 <algueracmp@gmail.com>
2022-03-11 00:23:32 -0600

    Update DBRedSocialSQL.sql

--- Bases de datos/DBRedSocialSQL.sql
+++ Bases de datos/DBRedSocialSQL.sql
@@ -14,6 +14,30 @@ create table Usuario
     hobbies varchar(200)
 ); 
 
+
+DELIMITER //
+create procedure insertUsuario(IN correoElectronico varchar(150), IN nombre varchar(50), IN apellido1 varchar(50),
+IN apellido2 varchar(50), IN fechaNacimiento date, IN clave varchar(200), IN intereses varchar (200), descripcionGeneral varchar (200),
+IN hobbiese varchar (200))
+BEGIN 
+	insert into Usuario values(correoElectronico,nombre,apellido1,apellido2,fechaNacimiento,clave,intereses,descripcionGeneral,hobbiese);
+END //
+DELIMITER ;
+-- Error de sintaxis--
+DELIMITER // 
+create procedure deleteTable (IN CorreoElectronico varchar(150))
+BEGIN 
+		delete from Usuario where correoElectronico= @CorreoElectronico;
+END //
+DELIMITER ;
+
+drop procedure insertTable;
+
+call insertTable('meguilu11@hotmail.com', 'Melissa', 'Alguera', 'Castillo', '2000-10-25', 'jacksonWang',
+ 'Kpop','Brilla Brilla estrellita', 'Bailar');
+ 
+ select * from Usuario;
+
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '534444';
 
 describe Usuario;
