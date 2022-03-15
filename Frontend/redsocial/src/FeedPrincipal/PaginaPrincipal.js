571ebdb0aef06711f7995d7af3c22c0ffa2c0f41
IssaAC00 <algueracmp@gmail.com>
2022-03-14 19:45:21 -0600

    Create PaginaPrincipal.js

---
+++ Frontend/redsocial/src/FeedPrincipal/PaginaPrincipal.js
@@ -0,0 +1,13 @@
+
+
+import Sidebar from '../Components/Sidebar'
+
+import React from 'react'
+
+export default function PaginaPrincipal() {
+  return (
+    <div>PaginaPrincipal
+    <Sidebar>werwerwerwr</Sidebar>
+    </div>
+  )
+}
