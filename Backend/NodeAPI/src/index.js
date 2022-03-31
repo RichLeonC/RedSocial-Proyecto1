b91925943f60e1f1ae32be1b161d88cd07c9eb13
IssaAC00 <algueracmp@gmail.com>
2022-03-31 14:08:43 -0600

    update
    firebase

--- Backend/NodeAPI/src/index.js
+++ Backend/NodeAPI/src/index.js
@@ -6,12 +6,14 @@ const postsRoute = require("./routes/posts.js");
 const app = express();
 const mongoose = require('./Databases/dbMongo');
 
+const app1 = require('./routes/firebasechat.js');
+
 //Settings
 app.set('port',process.env.PORT||3000); //Setea una variable port, el cual es la que nos puede proveer un SO, caso contrario puerto 3000
 
 // Middlewares (son funciones que se ejecutan antes de que se procese algo)
 app.use(express.json()); //Hace que el servidor entienda formato JSON, para poder accederlo
-
+545454
 //app.use(helmet()); //Ayuda a la seguridad 
 app.use(morgan("common"));
 //Routes (son las url's del servidor)
@@ -22,3 +24,7 @@ app.use("/posts",postsRoute);
 app.listen(app.get('port'),()=>{ //Va abrir el server en el puerto 3000
     console.log('Server on port',app.get('port'));
 })
+
+app1.listen(4000)
+
+console.log('Server on port 4000'); 
