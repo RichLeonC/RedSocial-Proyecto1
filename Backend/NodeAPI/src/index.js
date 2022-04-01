b55e065b3c0962ff2873832376d4218b178fbe9a
RichLeonC <r29leonc@gmail.com>
2022-03-31 21:39:37 -0600

    Update fronted and backend

--- Backend/NodeAPI/src/index.js
+++ Backend/NodeAPI/src/index.js
@@ -5,18 +5,39 @@ const usuariosRoute = require("./routes/usuarios");
 const postsRoute = require("./routes/posts.js");
 const app = express();
 const mongoose = require('./Databases/dbMongo');
+const multer = require('multer') //Para cargar archivos
+const path = require('path');
 
 //Settings
 app.set('port',process.env.PORT||3000); //Setea una variable port, el cual es la que nos puede proveer un SO, caso contrario puerto 3000
 
 // Middlewares (son funciones que se ejecutan antes de que se procese algo)
 app.use(express.json()); //Hace que el servidor entienda formato JSON, para poder accederlo
-
-//app.use(helmet()); //Ayuda a la seguridad 
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({ //Donde se va a almacenar el archivo cargado
+    destination:(req,file,cb)=>{
+        cb(null,"public/images")
+    },
+    filename:(req,file,cb)=>{
+        cb(null,req.body.name);
+    }
+})
+
+const upload = multer({storage});
+app.post('/upload',upload.single('file'),(req,res)=>{ //Carga el archivo
+    try{
+        return res.status(200).json('Archivo cargado correctamente')
+    }catch(error){
+        console.log(error);
+    }
+})
+
 //Routes (son las url's del servidor)
 app.use("/usuarios",usuariosRoute);
 app.use("/posts",postsRoute);
+app.use("/images",express.static(path.join(__dirname,'../public/images')));//Para acceder a las imagenes desde la URL
+
 
 //Starting the server
 app.listen(app.get('port'),()=>{ //Va abrir el server en el puerto 3000
