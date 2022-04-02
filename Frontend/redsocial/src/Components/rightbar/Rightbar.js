66e928b3c1054b0838999abc57fca21d8e92d254
RichLeonC <r29leonc@gmail.com>
2022-04-01 22:19:20 -0600

    Update

--- Frontend/redsocial/src/Components/rightbar/Rightbar.js
+++ Frontend/redsocial/src/Components/rightbar/Rightbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './rightbar.css'
-import Chat from '../chat/Chat';
+import Chat from '../chat/chat';
 
 
 export default function Rightbar() {
   return (
     <div className="rightbar">
     <div className="rightbarWrapper">
-    <Chat></Chat>
+
     </div>
   </div>
   )
