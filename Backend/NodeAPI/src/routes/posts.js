35c17d27f1c86d455c5a937631d030d225fe444e
RichLeonC <r29leonc@gmail.com>
2022-06-10 18:42:14 -0600

    Update edicion post

--- Backend/NodeAPI/src/routes/posts.js
+++ Backend/NodeAPI/src/routes/posts.js
@@ -53,7 +53,7 @@ router.put("/:id", async (req, res) => {
         await post.updateOne({ $set: req.body });
         res.status(200).json("Post actualizado");
       } else {
-        res.status(403).json("Solo puedes mofiicar tu post");
+        res.status(403).json("Solo puedes modificar tu post");
       }
     } catch (err) {
       res.status(500).json(err);
